09fd1cea2942d7745c0fc4f99e948586537ad897
IIYAMA12 <jorik@012.156.028.145.hva.nl>
2018-02-20 12:43:22 +0100

    not direct input feedback

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+
+// dialog opener
 (function(){
     const dialogElementOpen = document.querySelector('button[name="dialog-open"]');
     const focusOnDialog = function () {
@@ -21,3 +23,19 @@ setTimeout(function () {
     const loader = document.querySelector("[aria-label=\"loader\"]");
     loader.parentElement.removeChild(loader);
 }, 4000);
+
+
+const inputFields = document.querySelectorAll("[type=\"email\"], [type=\"password\"]");
+
+const enableInputFeedback = function (e) {
+    const source = e.target;
+    source.removeEventListener("focus", enableInputFeedback);
+    setTimeout(function () {
+        source.setAttribute("used", true);
+    }, 1000);
+};
+
+for (let i = 0; i < inputFields.length; i++) {
+    const inputField = inputFields[i];
+    inputField.addEventListener("focus", enableInputFeedback, false);
+}
